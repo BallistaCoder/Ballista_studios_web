3e7e63e3111387a877735c1612cc23983a209d09
BallistaCoder <87249697+BallistaCoder@users.noreply.github.com>
2021-08-13 00:19:37 +1200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 var mode = 0;
 mode = mode + 1;
 function mode_swap() {
-  if mode == 0 {
+  if (mode == 0) {
     document.body.style.backgroundColor = "white";
   }
-  if mode == 1 {
+  if (mode == 1) {
     document.body.style.backgroundColor = "#0f0f0f";
   }
-  if mode == 2 {
+  if (mode == 2) {
      document.body.style.backgroundColor = "white";
      mode = 0
   }
