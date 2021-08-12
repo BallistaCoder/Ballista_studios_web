d09a3e1b6506d8dbd16ad1ceb4f1e66840cdf150
BallistaCoder <87249697+BallistaCoder@users.noreply.github.com>
2021-08-13 00:16:33 +1200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+var mode = 0;
+function mode_swap() {
+  if mode == 0 {
+    document.body.style.backgroundColor = "white";
+  }
+  if mode == 1 {
+    document.body.style.backgroundColor = "#0f0f0f";
+  }
+}
