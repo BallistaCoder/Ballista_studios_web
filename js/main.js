da56abfa9c92f98616ecfc1e0415a5c550bf5725
BallistaCoder <87249697+BallistaCoder@users.noreply.github.com>
2021-08-13 12:20:06 +1200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 try {
-  var mode = localstorage.key("bg");
+  var mode = sessionStorage.key("bg");
   mode_swap();
   }
 catch {
   var mode = 1;
-  window.localStorage.setItem("bg", mode);
+  sessionStorage.setItem("bg", mode);
 }
 
 function mode_swap() {
   if (mode == 0){
     document.body.style.backgroundColor = "white";
     mode = 1;
-    window.localStorage.setItem("bg", mode);
+    sessionStorage.setItem("bg", mode);
   }
   else if (mode == 1) {
     document.body.style.backgroundColor = "#0f0f0f";
     mode = 0
-    window.localStorage.setItem("bg", mode);
+    sessionStorage.setItem("bg", mode);
   }
 }
