4201a16ce530f8a22ea44ade4670235fee0e9b1d
BallistaCoder <87249697+BallistaCoder@users.noreply.github.com>
2021-08-13 14:00:39 +1200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 var mode = localStorage.getItem("bg");
-alert("test");
+mode_swap();
 if (mode == null) {
   var mode = "dark";
   localStorage.setItem("bg", mode);
   mode_swap();
   alert("test1");
-} else {
-  mode_swap();
-  alert("test2");
 }
 
 function mode_swap() {
