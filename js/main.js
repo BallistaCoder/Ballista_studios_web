5576d334294769ca75961b875d10c947341eb1a4
BallistaCoder <87249697+BallistaCoder@users.noreply.github.com>
2021-08-15 11:32:28 +1200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
-var mode = localStorage.getItem("bg");
-mode_swap();
-if (mode == null) {
-  var mode = "dark";
-  localStorage.setItem("bg", mode);
-  mode_swap();
-  alert("test1");
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
 }
+// function to toggle between light and dark theme
+function toggleTheme() {
+   if (localStorage.getItem('theme') === 'theme-dark'){
+       setTheme('theme-light');
+   } else {
+       setTheme('theme-dark');
+   }
+}
+// Immediately invoked function to set the theme on initial load
+(function () {
+   if (localStorage.getItem('theme') === 'theme-dark') {
+       setTheme('theme-dark');
+   } else {
+       setTheme('theme-light');
+   }
+})();
 
-function mode_swap() {
-  if (mode == "light"){
-    document.body.style.backgroundColor = "white";
-    mode = "dark";
-    localStorage.setItem("bg", mode);
-  }
-  else if (mode == "dark") {
-    document.body.style.backgroundColor = "#0f0f0f";
-    mode = "light";
-    localStorage.setItem("bg", mode);
-  }
-}
